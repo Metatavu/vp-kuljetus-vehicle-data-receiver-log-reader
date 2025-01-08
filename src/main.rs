75868251e12606820372c82a819b2e995ff588bb
Ville Juutila <ville.juutila@gmail.com>
2025-01-08 14:50:40 +0200

    Changed to deserialize JSON

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use std::io::{BufWriter, Write};
+use std::{fs::{create_dir_all, OpenOptions}, io::{BufWriter, Write}, path::Path};
 
 use base64::Engine;
-use nom_teltonika::{parser::tcp_frame, AVLFrame};
+use nom_teltonika::AVLFrame;
+use chrono::Timelike;
 
 /// [VP-Kuljetus Vehicle Data Receiver](https://www.github.com/metatavu/vp-kuljetus-vehicle-data-receiver) can be configured to write all incoming [`AVLFrame`]s to a file.
 ///
-/// This program reads those files and writes them into a JSON file.
+/// This program reads those files and writes them as JSON files in a directory structure based on the timestamp of each record.
 ///
 /// # Example
-/// `vp-kuljetus-vehicle-data-receiver-log-reader input.txt` outputs `input.json`
+/// `vp-kuljetus-vehicle-data-receiver-log-reader input.txt`
+///
+/// # Output
+/// ```
+/// - input
+///  ├── 0
+///     ├── {hour}:{minute}:{second}.{millisecond}.json
+///  ├── 1
+///     ├── {hour}:{minute}:{second}.{millisecond}.json
+///  |--- input.json
+/// ```
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
@@ -16,17 +27,19 @@ fn main() -> Result<(), std::io::Error> {
     }
     let input_filename = &args[1];
     let input_file_contents = std::fs::read_to_string(input_filename)?;
-    let output_filename = input_filename.replace(".txt", ".json");
+    let output_dirname = input_filename.replace(".txt", "");
+    create_dir_all(output_dirname.clone())?;
+    let output_filename = Path::new(&output_dirname).join(input_filename.replace(".txt", ".json"));
     let output_file = std::fs::File::create(&output_filename)?;
 
     let mut output_writer = BufWriter::new(output_file);
 
     let encoded_frames: Vec<&str> = input_file_contents.trim_start().split("\\n").filter(|x| !x.is_empty()).collect();
 
-    println!("Log contains {} frames", encoded_frames.len());
-    let mut frames: Vec<AVLFrame> = Vec::new();
+    let mut frames = Vec::new();
+    let mut records = Vec::new();
 
-    for (index, encoded_frame) in encoded_frames.iter().enumerate() {
+    for encoded_frame in encoded_frames.iter() {
         let decoded_frame = match base64::prelude::BASE64_STANDARD.decode(encoded_frame) {
             Ok(decoded) => decoded,
             Err(e) => {
@@ -34,10 +47,9 @@ fn main() -> Result<(), std::io::Error> {
                 continue;
             }
         };
-        let avl_frame = tcp_frame(&decoded_frame);
-        match avl_frame {
-            Ok((_, frame)) => {
-                println!("{}. Frame has {} records", index + 1, frame.records.len());
+        match serde_json::from_slice::<AVLFrame>(&decoded_frame) {
+            Ok(frame) => {
+                records.append(&mut frame.records.clone());
                 frames.push(frame);
             }
             Err(e) => {
@@ -46,9 +58,20 @@ fn main() -> Result<(), std::io::Error> {
             }
         }
     }
+
+    records.iter().for_each(|record| {
+        let dirname = output_dirname.clone();
+        let hour = record.timestamp.hour();
+        let filename = record.timestamp.format("%H:%M:%S.%3f").to_string();
+        let record_dir_path = format!("{}/{}", dirname, hour);
+        let record_file_path = format!("{}/{}.json", record_dir_path, filename);
+        create_dir_all(record_dir_path).unwrap();
+        let mut file = OpenOptions::new().create(true).write(true).open(record_file_path).unwrap();
+        file.write(serde_json::to_string_pretty(record).unwrap().as_bytes()).unwrap();
+    });
+
     serde_json::to_writer_pretty(&mut output_writer, &frames)?;
     output_writer.flush()?;
-
 
     Ok(())
 }
